01e09ad940d9f089c04bedecac3319458147d8e6
Andrea Lauria <andrea.lauria104@gmail.com>
2019-11-28 11:20:10 +0100

    add tools: gtf2bed, sam_to_bam

---
+++ gtf2bed.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+INFILE=$1
+OUTFILE="${INFILE%.gtf}.bed"
+
+echo "[*] Convert gtf to bed [*]"
+echo ""
+echo " - input file: $INFILE"
+echo " - output file: $OUTFILE"
+
+awk 'OFS="\t" {print $1,$4-1,$5,$10,$16,$7}' $INFILE | tr -d '";' > $OUTFILE
+
+echo ""
+echo "done."
---
+++ sam_to_bam.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+#
+# Convert SAM to sorted and indexed BAM.
+#
+# Function definition ---
+fsam2bam () {
+	tmpbam=${1%.*}.tmp.bam
+	outbam=${1%.*}.bam
+
+	echo "[*] Converting SAM to BAM ..."
+
+	if [ ! -f $1 ]; then
+		echo "$1 does not exist."
+		return 1
+	fi
+
+	samtools view -bS $1 > $tmpbam
+	if [ $? -ne 0 ]; then
+		echo "	[!] SAM2BAM conversion not sucessful."
+		echo "	[!] $1 remains unchanged."
+		rm $tmpbam
+		return 1
+	fi
+
+	echo "[*] Sorting BAM ..."
+	samtools sort -o $outbam $tmpbam 
+	if [ $? -ne 0 ]; then
+		echo "	[!] BAM file sorting not sucessful."
+		echo "	[!] $outbam is in unsorted BAM format".
+		mv $tmpbam $outbam
+		return 1
+	fi
+	rm $tmpbam
+
+	echo "[*] Indexing BAM ..."
+	samtools index $outbam
+	if [ $? -ne 0 ]; then
+		echo "	[!] BAM file indexing not sucessful."
+		return 1
+	fi
+
+	return 0
+}
+
+# execution ---
+INPUT=$1
+echo "============================================="
+echo "=== Convert SAM to sorted and indexed BAM ==="
+echo "============================================="
+echo "\n"
+
+if [ -f "${INPUT}" ]; then
+	fsam2bam $INPUT
+	echo "[*] done."
+elif [ -d "${INPUT}" ]; then
+	SAMDIR=$INPUT
+	BAMDIR="BAM"
+	mkdir $BAMDIR
+	for SAM in $SAMDIR/*.sam
+	do
+		echo " -- processing file: $SAM"
+		fsam2bam $SAM
+	done
+	echo " -- moving files"
+	mv -t $BAMDIR $SAMDIR/*.bam*
+	echo "[*] done."
+fi
