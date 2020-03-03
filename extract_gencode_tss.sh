8427ccab7da2088c4037f0a205b9da33792caf2c
Andrea Lauria <andrea.lauria104@gmail.com>
2020-03-03 15:46:03 +0100

    add extract tss

---
+++ extract_gencode_tss.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+ANNOT=$1
+BED=${2:-true}
+
+echo ""
+echo " [+] Extract TSS, annotation = ${ANNOT}"
+echo ""
+
+if [[ $BED == true ]]; then
+	OUTFILE=${ANNOT%.gtf}.tss.bed
+	echo "  -- output BED format (0-based), file = ${OUTFILE}"
+	echo ""
+	awk 'BEGIN{OFS="\t"};$3 == "gene" {
+		if($7 == "+") tss = $4;
+		else if($7 == "-") tss = $5;
+		gsub("\"|;","");
+		print $1,tss-1,tss,$7,$10}
+		' < $ANNOT > $OUTFILE
+else
+	OUTFILE=${ANNOT%.gtf}.tss.txt
+	echo "  -- output file = ${OUTFILE}"
+	echo ""
+	awk 'BEGIN{OFS="\t"};$3 == "gene" {
+		if($7 == "+") tss = $4;
+		else if($7 == "-") tss = $5;
+		gsub("\"|;","");
+		print $1,tss,$7,$10}
+		' < $ANNOT > $OUTFILE
+fi
+
+
