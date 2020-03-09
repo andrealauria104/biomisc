bc6e3064c9471404150682a0f3858748b9c7aa0f
Andrea Lauria <andrea.lauria104@gmail.com>
2020-03-09 18:55:00 +0100

    fix strandness

--- mutfilter.py
+++ mutfilter.py
@@ -10,6 +10,9 @@
 '''
 import pysam, re, subprocess, argparse, sys
 
+# Global ---
+alphabet = {"A":"T","T":"A","C":"G","G":"C"}
+
 # Function definitions ---
 def parse_mdtag(bampath):
 	'''
@@ -23,7 +26,7 @@ def parse_mdtag(bampath):
 		if read.is_unmapped:
 			continue
 		md = read.get_tag("MD")
-		mdtags.update({read.query_name: (read.query_sequence, md)})
+		mdtags.update({read.query_name: {'md':md, 'seq':read.query_sequence, 'reverse':read.is_reverse}})
 
 	bamfile.close()
 
@@ -37,17 +40,16 @@ def get_mismatch(tagsv):
 	- list of mismatches in the form ref=>alt
 	- MD tag
 	- Sequence in read
+	- Is reverse-complemented
 	'''
-	md  = tagsv[1]
-	seq = tagsv[0]
 
 	dd = list()
 	nt = list()
 
-	for match in re.finditer(r"\d+",md):
+	for match in re.finditer(r"\d+",tagsv['md']):
 		dd.append((int(match.group()), match.start()))
 
-	for match in re.finditer(r"[A,T,C,G]",md):
+	for match in re.finditer(r"[A,T,C,G]",tagsv['md']):
 	    nt.append((match.group(), match.start()))
 
 	mismatch = list()
@@ -55,26 +57,39 @@ def get_mismatch(tagsv):
 	pos = 0
 	for i in range(0,len(nt)):
 		pos += dd[i][0]
-		mismatch.append(nt[i][0] + '=>' + seq[pos])
+		mismatch.append(nt[i][0] + '=>' + tagsv['seq'][pos])
 		pos += 1
 			
-	return (mismatch, md, seq)
+	return (mismatch, tagsv)
 
 
-def filter_mismatch(mismatch, mutation, report_exact_matches):
+def filter_mismatch(mismatch, mutation, report_exact_matches, directional):
 	'''
 	# Filter mismatch for specific mutations
 	'''
-	
-	if report_exact_matches:
-		filtmismatch = all(x == mutation for x in mismatch[0]) or len(mismatch[0])==0
+	reverse_mutation = alphabet[mutation.split('=>')[0]] + '=>' + alphabet[mutation.split('=>')[1]]
+
+	if report_exact_matches == True:
+		if directional == True:
+			if mismatch[1]['reverse'] == True:
+				filtmismatch = all(x == reverse_mutation for x in mismatch[0]) or len(mismatch[0])==0
+			else:
+				filtmismatch = all(x == mutation for x in mismatch[0]) or len(mismatch[0])==0
+		else:
+			filtmismatch = all(x == mutation for x in mismatch[0]) or len(mismatch[0])==0
 	else:
-		filtmismatch = all(x == mutation for x in mismatch[0]) and len(mismatch[0])!=0
+		if directional == True:
+			if mismatch[1]['reverse'] == True:
+				filtmismatch = all(x == reverse_mutation for x in mismatch[0]) and len(mismatch[0])!=0
+			else:
+				filtmismatch = all(x == mutation for x in mismatch[0]) and len(mismatch[0])!=0
+		else:
+			filtmismatch = all(x == mutation for x in mismatch[0]) and len(mismatch[0])!=0
 
 	return filtmismatch
 
 
-def filter_bam(bampath, mutation, report_exact_matches):
+def filter_bam(bampath, mutation, report_exact_matches, directional):
 	'''
 	# Filter and write to BAM file
 	# Sort and index BAM
@@ -91,12 +106,12 @@ def filter_bam(bampath, mutation, report_exact_matches):
 	for read in bamfile.fetch():
 		if read.is_unmapped:
 			continue
-		md  = read.get_tag("MD")
-		seq = read.query_sequence
-
-		mismatch = get_mismatch((seq, md))
+		# create input for mismatch retrieval: MD tag, sequence, strandness
+		tagsv = {"md": read.get_tag("MD"), "seq": read.query_sequence, "reverse": read.is_reverse}
 		
-		if filter_mismatch(mismatch, mutation, report_exact_matches):
+		mismatch = get_mismatch(tagsv)
+		
+		if filter_mismatch(mismatch, mutation, report_exact_matches, directional):
 			filtbamfile.write(read)
 
 	filtbamfile.close()
@@ -137,12 +152,18 @@ def create_parser():
 		, dest='report_exact_matches'
 		, action='store_true'
 		, default=None
-		, help='Report reads with no mismatches.')
+		, help='Include reads with no mutations.')
+
+	parser.add_argument('-d','--directional'
+		, dest='directional'
+		, action='store'
+		, default=True
+		, help='Assume directional (stranded) library (Default: True).')
 
 	return parser
 
 
-def main(bampath, mutation, stdout, report_exact_matches):
+def main(bampath, mutation, stdout, report_exact_matches, directional):
 	'''
 	Main program function
 	'''
@@ -152,13 +173,16 @@ def main(bampath, mutation, stdout, report_exact_matches):
 		# Print mutations to stdout
 		mdtags   = parse_mdtag(bampath)
 		mismatch = dict((k, get_mismatch(v)) for k,v in mdtags.items())
-
+		print(report_exact_matches)
+		print(mutation)
 		for k,v in mismatch.items():
-			print(k, " :: ", v)
-
+			if filter_mismatch(v, mutation, report_exact_matches, directional):
+				print(k, " :: ", v)
+			else:
+				continue
 	else:
 		# Write filtered BAM
-		filter_bam(bampath, mutation, report_exact_matches)
+		filter_bam(bampath, mutation, report_exact_matches, directional)
 
 	sys.exit("\n[*] done.\n")
 
@@ -178,4 +202,5 @@ if __name__ == '__main__':
 	main(bampath = args.bampath
 		, mutation = args.mutation
 		, stdout = args.stdout
-		, report_exact_matches = args.report_exact_matches)
+		, report_exact_matches = args.report_exact_matches
+		, directional = args.directional)
